f7edd66f0d55cabd82d006b5c78733c5c12cf123
Hyperlox <reesejtr@gmail.com>
2022-08-12 17:15:30 -0500

    Update SortingAlgs.java

--- SortingAlgs.java
+++ SortingAlgs.java
@@ -67,60 +67,125 @@ public class SortingAlgs
     }  
 
     //merge sort 
-    public static void mergeSort(int[] data)
-    {
-        mergeSort(data, 0, data.length-1);
+//     public static void mergeSort(int[] data)
+//     {
+//         mergeSort(data, 0, data.length-1);
+//     }
+    
+//     private static void mergeSort(int[] data, int min, int max)
+//     {
+//         if (min < max) //stops the recursion when there is only one value in the array
+//         {
+//             int mid = (min + max) / 2;
+//             //splits the array into two 
+//             mergeSort(data, min, mid);
+//             mergeSort(data, mid + 1, max);
+//             merge(data, min, mid, max);
+//         }
+//     }
+
+//     private static void merge(int[] data, int first, int mid, int last)
+//     {
+//         int[] temp = new int[data.length]; //creates a temporary array to put items in
+//         int first1 = first, last1 = mid; //endpoints of first subarray
+//         int first2 = mid+1, last2 = last; //endpoints of second subarray
+//         int index = first1; //index while putting values in the temporary array
+//         //Copies smaller item of the two arrays into the temporary array until one of the subarrays is used up
+//         while (first1 <= last1 && first2 <= last2)
+//         {
+//             if (data[first1] < data[first2])
+//             {
+//                 temp[index] = data[first1];
+//                 first1++;
+//             }
+//             else
+//             {
+//                 temp[index] = data[first2];
+//                 first2++;
+//             }
+//             index++;
+//         }
+//         //copies remaining elements from first subarray, if any
+//         while (first1 <= last1)
+//         {
+//             temp[index] = data[first1];
+//             first1++;
+//             index++;
+//         }
+//         while (first2 <= last2)
+//         {
+//             temp[index] = data[first2];
+//             first2++;
+//             index++;
+//         }
+//         //copies merged data into original array
+//         for (index = first; index <= last; index++)
+//             data[index] = temp[index];
+//     }
+    
+    private static void merge(int a[], int beg, int mid, int end)    
+    {    
+        int i, j, k;  
+        int n1 = mid - beg + 1;    
+        int n2 = end - mid;    
+
+       /* temporary Arrays */  
+            int LeftArray[] = new int[n1];  
+            int RightArray[] = new int[n2];  
+
+        /* copy data to temp arrays */  
+        for (i = 0; i < n1; i++)    
+        LeftArray[i] = a[beg + i];    
+        for (j = 0; j < n2; j++)    
+        RightArray[j] = a[mid + 1 + j];    
+
+        i = 0; /* initial index of first sub-array */  
+        j = 0; /* initial index of second sub-array */   
+        k = beg;  /* initial index of merged sub-array */  
+
+        while (i < n1 && j < n2)    
+        {    
+            if(LeftArray[i] <= RightArray[j])    
+            {    
+                a[k] = LeftArray[i];    
+                i++;    
+            }    
+            else    
+            {    
+                a[k] = RightArray[j];    
+                j++;    
+            }    
+            k++;    
+        }    
+        while (i<n1)    
+        {    
+            a[k] = LeftArray[i];    
+            i++;    
+            k++;    
+        }    
+
+        while (j<n2)    
+        {    
+            a[k] = RightArray[j];    
+            j++;    
+            k++;    
+        }    
+    }    
+    
+    private static void mergeSort(int a[], int beg, int end)  
+    {  
+        if (beg < end)   
+        {  
+            int mid = (beg + end) / 2;  
+            mergeSort(a, beg, mid);  
+            mergeSort(a, mid + 1, end);  
+            merge(a, beg, mid, end);  
+        }  
     }
     
-    private static void mergeSort(int[] data, int min, int max)
+    public static void mergeSort(int a[])
     {
-        if (min < max) //stops the recursion when there is only one value in the array
-        {
-            int mid = (min + max) / 2;
-            //splits the array into two 
-            mergeSort(data, min, mid);
-            mergeSort(data, mid + 1, max);
-            merge(data, min, mid, max);
-        }
-    }
-
-    private static void merge(int[] data, int first, int mid, int last)
-    {
-        int[] temp = new int[data.length]; //creates a temporary array to put items in
-        int first1 = first, last1 = mid; //endpoints of first subarray
-        int first2 = mid+1, last2 = last; //endpoints of second subarray
-        int index = first1; //index while putting values in the temporary array
-        //Copies smaller item of the two arrays into the temporary array until one of the subarrays is used up
-        while (first1 <= last1 && first2 <= last2)
-        {
-            if (data[first1] < data[first2])
-            {
-                temp[index] = data[first1];
-                first1++;
-            }
-            else
-            {
-                temp[index] = data[first2];
-                first2++;
-            }
-            index++;
-        }
-        //copies remaining elements from first subarray, if any
-        while (first1 <= last1)
-        {
-            temp[index] = data[first1];
-            first1++;
-            index++;
-        }
-        while (first2 <= last2)
-        {
-            temp[index] = data[first2];
-            first2++;
-            index++;
-        }
-        //copies merged data into original array
-        for (index = first; index <= last; index++)
-            data[index] = temp[index];
+        mergeSort(a, 0, a.length - 1);
     }
 
     //for driver method
